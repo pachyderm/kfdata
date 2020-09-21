27b052dffb88c93189be2e4e013c2011ceb95c58
Luke Marsden <me@lukemarsden.net>
2020-09-21 17:47:14 +0100

    bump VM size to cope with noobaa thingie in dlf demo; also install pachyderm

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -euo pipefail
-testctl get --config .testfaster.yml --drop-existing
+testctl get --config .testfaster.yml
 export KUBECONFIG=$(pwd)/kubeconfig
 export VERSION="$(git rev-parse HEAD)"
-export IMG=ghcr.io/pachyderm/kfdata:$VERSION
-(
-    cd src/kfdata
-    make docker-build docker-push
-    kubectl create namespace kfdata-system
-    make deploy
-)
+#export IMG=ghcr.io/pachyderm/kfdata:$VERSION
+#(
+#    cd src/kfdata
+#    make docker-build docker-push
+#    kubectl create namespace kfdata-system
+#    make deploy
+#)
+(cd dataset-lifecycle-framework; make minikube-install)

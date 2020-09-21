7583d8f90a9fdf2682fc45a094a77645462b81f0
Luke Marsden <me@lukemarsden.net>
2020-09-21 18:32:06 +0100

    move dlf setup into prewarm phase

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 testctl get --config .testfaster.yml
 export KUBECONFIG=$(pwd)/kubeconfig
 export VERSION="$(git rev-parse HEAD)"
+
+# NB: taking different approach for now, using dataset-lifecycle-framework...)
 #export IMG=ghcr.io/pachyderm/kfdata:$VERSION
 #(
 #    cd src/kfdata
@@ -10,4 +12,8 @@ export VERSION="$(git rev-parse HEAD)"
 #    kubectl create namespace kfdata-system
 #    make deploy
 #)
-(cd dataset-lifecycle-framework; make minikube-install)
+
+# NB: Following line is moved into .testfaster.yml
+#(cd dataset-lifecycle-framework; make minikube-install)
+
+(cd dataset-lifecycle-framework/examples/noobaa; ./noobaa_install.sh)
